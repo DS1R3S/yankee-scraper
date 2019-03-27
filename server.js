204a07c37b8c00b4212ccbadd6f070110b235183
DS1R3S <dsires13@mac.com>
2019-03-26 22:09:15 -0400

    repo

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/yankees", { useNewUrlParser: true });
 
@@ -32,27 +33,57 @@ mongoose.connect("mongodb://localhost/yankees", { useNewUrlParser: true });
 // Get route for scraping yankees website
 app.get('/scrape', function (req, res) {
     axios.get('https://www.mlb.com/yankees/news').then(function (response) {
+
         var $ = cheerio.load(response.data);
 
         $('li.article-navigation__item').each(function (i, element) {
             var result = {};
-            result.title = $(this).find('span').text().replace('\n',' ').trim();
-            result.link = $(this).children().attr('href');
+            
+            result.title = $(this).find('span').text().replace('\n', ' ').trim();
+            result.link = 'https://www.mlb.com/yankees' + $(this).children().attr('href');
 
-            db.Article.create(result).then(function (dbArticle) {
-                console.log(dbArticle);
-            }).catch(function (err) {
-                console.log(err);
-            });
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
 
         });
-        console.log(err);
+        res.send('Scrape Complete')
     });
-    console.log(result);
-})
+});
+
+app.get('/articles', function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
-  
+});
+
+
+// axios.get('https://www.mlb.com/yankees/news').then(function (response) {
+//     var $ = cheerio.load(response.data);
+
+//     var result = []
+//     $('li.article-navigation__item').each(function (i, element) {
+//         var title = $(element).find('span').text().replace('\n', ' ').trim();
+//         var link = $(element).children().attr('href');
+//         link = 'https://www.mlb.com/yankees'+ link;
+
+//         result.push({
+//             title: title,
+//             link: link
+//         })
+//     });
+//     console.log(result);
+// });
